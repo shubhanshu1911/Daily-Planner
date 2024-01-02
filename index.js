4188cad7bb8284c06ecceaa627b9a0295f6bbdb0
shubhanshu1911 <shubhkhare19@gmail.com>
2024-01-02 13:54:36 +0530

    Port Changed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 import _ from "lodash";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // let items = []; // store the items to be added
 let currentDay = null;
 
